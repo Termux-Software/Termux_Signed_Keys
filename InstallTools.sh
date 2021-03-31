866c6f3332a88fedc4ffe4805da30dc62deae7f2
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-03-30 23:04:24 -0400

    Update InstallTools.sh

--- InstallTools.sh
+++ InstallTools.sh
@@ -5,8 +5,8 @@ echo
 echo -e "\e[93mThis script will Sign Android Apk Keys in Termux
 echo -e "\e[32m[*] \e[34mDownloading binaries..."
 mkdir Signed Keys Output Folder
-wget https://github.com/lc67846/Termux_Signed_Keys/raw/master/bin/Signing Key.x509.pem -P $Signed Keys Output Folder/ -q
-wget https://github.com/lc67846/Termux_Signed_Keys/raw/master/bin/Signing Key.pk8 -P $Signed Keys Output Folder/ -q
+wget https://github.com/lc67846/Termux_Signed_Keys/raw/master/bin/Signing Key.x509.pem -m $Signed Keys Output Folder/ -M
+wget https://github.com/lc67846/Termux_Signed_Keys/raw/master/bin/Signing Key.pk8 -m $Signed Keys Output Folder / -m
  -e "\e[32m[*] \e[34mCleaning Up Signing Traces..."
 cd $directory
 rm -rf InstallTools.sh
